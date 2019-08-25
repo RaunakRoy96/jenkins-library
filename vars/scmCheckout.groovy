d468ccbe4da7e690ed228cd01d7c8b14bf0f9045
RaunakRoy96 <royraunak96@gmail.com>
2019-08-26 01:24:58 +0530

    Update scmCheckout.groovy

--- vars/scmCheckout.groovy
+++ vars/scmCheckout.groovy
@@ -1,11 +1,11 @@
 class CheckoutProperties {
-	scmClass = 'GitSCM';
-	branchName = '*/master';
-	scmUrl = 'https://github.com/RaunakRoy96/journaldev-springboot.git';
+	string scmClass = 'GitSCM';
+	string branchName = '*/master';
+	string scmUrl = 'https://github.com/RaunakRoy96/journaldev-springboot.git';
 }
 
-def call() {
-	checkout(
+def call(script) {
+	script.checkout(
         [
             $class: ${CheckoutProperties.scmClass}, 
             branches: [[name: ${CheckoutProperties.branchName}]], 
