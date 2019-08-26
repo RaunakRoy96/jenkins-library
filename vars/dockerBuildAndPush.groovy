737c3b9a44cafef948e72b536b4325403167f0d6
Raunak Roy <royraunak96@gmail.com>
2019-08-26 13:45:47 +0530

    made corrections

--- vars/dockerBuildAndPush.groovy
+++ vars/dockerBuildAndPush.groovy
@@ -1,7 +1,7 @@
 def registry = 'https://registry-1.docker.io/v2/'
 
 
-def call(scirpt ,imageNameWithUserid ,credentials ) {
+def call(script ,imageNameWithUserid ,credentials ) {
      script.docker.withRegistry(registry, credentials) {
             script.docker.build(imageNameWithUserid).push()
        }
