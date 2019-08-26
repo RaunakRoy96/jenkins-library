e3fd3ba36538286f39736ce096117873b0625095
Raunak Roy <royraunak96@gmail.com>
2019-08-26 13:29:24 +0530

    added docker libraries

---
+++ vars/dockerBuildAndPush.groovy
@@ -0,0 +1,8 @@
+def registry = 'https://registry-1.docker.io/v2/'
+
+
+def call(scirpt ,imageNameWithUserid ,credentials ) {
+     script.docker.withRegistry(registry, credentials) {
+            script.docker.build(credentials).push()
+       }
+}
--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -4,7 +4,7 @@ def call(script, jdkVersion, mavenVar) {
             sh "mvn sonar:sonar -Dsonar.projectKey=key -Dsonar.login=19bb76801ffeda4139739c041ebe2663e44163d0"
         }
     }
-    script.timeout(time: 1, unit: 'HOURS') {
+    script.timeout(time: 3, unit: 'MINUTES') {
         def qualitygate = script.waitForQualityGate()
             if (qualitygate.status != "OK") {
                 error "Pipeline aborted due to quality gate coverage failure: ${qualitygate.status}"
