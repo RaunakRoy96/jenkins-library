7b7aa3b22fccbaa642c8fd2495b3c0400ec2180a
Raunak Roy <royraunak96@gmail.com>
2019-08-26 15:08:36 +0530

    made corrections

--- vars/dockerBuildAndPush.groovy
+++ vars/dockerBuildAndPush.groovy
@@ -2,7 +2,7 @@ registry = 'https://registry-1.docker.io/v2/'
 
 
 def call(script ,imageNameWithUserid ,credentials ) {
-     script.docker.withRegistry(this.registry, credentials) {
+     script.docker.withRegistry(registry, credentials) {
             script.docker.build(imageNameWithUserid).push()
        }
 }
--- vars/dockerPullAndRun.groovy
+++ vars/dockerPullAndRun.groovy
@@ -1,7 +1,7 @@
 registry = 'https://registry-1.docker.io/v2/'
 
 def call(script, imageNameWithUserid, credentials, port1, port2) {
-    script.docker.withRegistry(this.registry, credentials) {
+    script.docker.withRegistry(registry, credentials) {
            script.docker.image(imageNameWithUserid).run("-p $(port1):$(port2)")
        }
 }
