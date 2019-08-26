8c68e3533aac7a93b2d82aea0027b26d8fe23251
Raunak Roy <royraunak96@gmail.com>
2019-08-26 09:22:44 +0530

    merging

--- vars/scmCheckout.groovy
+++ vars/scmCheckout.groovy
@@ -1,17 +1,18 @@
 class CheckoutProperties {
-	scmClass = 'GitSCM';
-	branchName = '*/master';
-	scmUrl = 'https://github.com/RaunakRoy96/journaldev-springboot.git';
+	static String scmClass = 'GitSCM';
+	static String branchName = '*/master';
+	static String scmUrl = 'https://github.com/RaunakRoy96/journaldev-springboot.git';
 }
 
-def call() {
-	checkout(
+def call(script) {
+	script.checkout(
         [
-            $class: ${CheckoutProperties.scmClass}, 
-            branches: [[name: ${CheckoutProperties.branchName}]], 
+            $class: CheckoutProperties.scmClass, 
+            branches: [[name: CheckoutProperties.branchName]], 
             doGenerateSubmoduleConfigurations: false, 
             extensions: [], 
             submoduleCfg: [], 
-            userRemoteConfigs: [[url: ${CheckoutProperties.scmUrl}]]
+            userRemoteConfigs: [[url: CheckoutProperties.scmUrl]]
         ]
+    )
 }
