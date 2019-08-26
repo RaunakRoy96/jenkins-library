1e584071e154b87516fe391015bac46124de04aa
Raunak Roy <royraunak96@gmail.com>
2019-08-26 15:22:29 +0530

    made corrections

--- vars/dockerPullAndRun.groovy
+++ vars/dockerPullAndRun.groovy
@@ -1,5 +1,5 @@
 def call(script, imageNameWithUserid, credentials, port1, port2) {
     script.docker.withRegistry('https://registry-1.docker.io/v2/', credentials) {
-           script.docker.image(imageNameWithUserid).run("-p $(port1):$(port2)")
+           script.docker.image(imageNameWithUserid).run("-p ${port1}:${port2}")
        }
 }
