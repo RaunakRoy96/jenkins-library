57102f3e93c4d50592f14d77f1470dd640a95e3d
Raunak Roy <royraunak96@gmail.com>
2019-08-29 17:56:48 +0530

    modified

--- vars/deployWithAnsible.groovy
+++ vars/deployWithAnsible.groovy
@@ -1,6 +1,8 @@
 def call(script, host) {
     script.withCredentials([usernamePassword(credentialsId: '47f84f4d-ab54-4743-8250-875b54c8bab9', passwordVariable: 'dockerPassword', usernameVariable: 'dockerUsername')]) {
         //sh "ansible-playbook DeployDocker.yml  -vvvv --extra-vars 'HOST=${host} USERNAME=${env.dockerUsername} PASSWORD=${env.dockerPassword}'"
-        sht "ansible -m ping all-e 'ansible_user=devopsinfra ansible_ssh_pass=Applestore12\$6 ansible_sudo_pass=Applestore12\$6'
+        def user = "devopsinfra"
+        def pass = "Applestore12\$6"
+        sht "ansible -m ping all-e 'ansible_user=${user} ansible_ssh_pass=${pass} ansible_sudo_pass=${pass}'
     }
 }
