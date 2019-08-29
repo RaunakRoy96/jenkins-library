f3411c65812126ae8159646ee6e072dcf37bc608
Raunak Roy <royraunak96@gmail.com>
2019-08-29 15:21:29 +0530

    modified

--- vars/deployWithAnsible.groovy
+++ vars/deployWithAnsible.groovy
@@ -1,5 +1,5 @@
 def call(script, host) {
     script.withCredentials([usernamePassword(credentialsId: '47f84f4d-ab54-4743-8250-875b54c8bab9', passwordVariable: 'dockerPassword', usernameVariable: 'dockerUsername')]) {
-        sh "ansible-playbook DeployDocker.yml --key-file 'id_rsa' -vvvv --extra-vars 'HOST=${host} USERNAME=${env.dockerUsername} PASSWORD=${env.dockerPassword}'"
+        sh "ansible-playbook DeployDocker.yml  -vvvv --extra-vars 'HOST=${host} USERNAME=${env.dockerUsername} PASSWORD=${env.dockerPassword}'"
     }
 }
